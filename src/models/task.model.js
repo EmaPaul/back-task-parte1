298c6d1af7a297ea0586a6263e6f2d3e3c3b7746
EmaPaul <enmanuelpaulcarrillo.rakata900@gmail.com>
2024-12-02 15:51:25 -0500

    correction_unique

--- src/models/task.model.js
+++ src/models/task.model.js
@@ -4,13 +4,11 @@ const taskSchema = mongoose.Schema({
     title: {
         type: String,
         required: true,
-        unique:true
     },
 
     description: {
         type: String,
         required:true,
-        unique:true,
     },
 
     status:{
